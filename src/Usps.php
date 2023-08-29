a45b9fe2a227ace050073b5964fd3b017060c098
Cory DeMille <cory@corydemille.com>
2023-08-29 00:28:10 -0600

    added docblock

--- src/Usps.php
+++ src/Usps.php
@@ -114,11 +114,7 @@ class Usps {
             
         
     }
-
-    /**
-     * @param $request
-     * @return array
-     */
+    
     public function cityStateLookup($request){
         $verify = new CityStateLookup($this->config['username']);
         $verify->addZipCode((array_key_exists('Zip', $request) ? $request['Zip'] : null ));

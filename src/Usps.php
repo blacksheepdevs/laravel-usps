c70f0e6b2da9872f08e7e56aa9fba745a626f5f1
Cory DeMille <cory@corydemille.com>
2023-08-29 15:18:56 -0600

    Updated response

--- src/Usps.php
+++ src/Usps.php
@@ -130,13 +130,15 @@ class Usps {
         $val1 = $verify->lookup();
         $val2 = $verify->getArrayResponse();
 
+        var_dump($val2);
+
         // var_dump($verify->isError());
 
         // See if it was successful
-        if ($verify->isSuccess()) {
-            return ['address' => $val2['AddressValidateResponse']['Address']];
-        } else {
-            return ['error' => $verify->getErrorMessage()];
-        }
+//        if ($verify->isSuccess()) {
+//            return ['address' => $val2['AddressValidateResponse']['Address']];
+//        } else {
+//            return ['error' => $verify->getErrorMessage()];
+//        }
     }
 }
